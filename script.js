2742640b85e5a9f6fb865093cdd2e49a9765ff2d
Derick Luiz <derick.stos@gmail.com>
2025-11-12 16:34:58 -0300

    #

---
+++ script.js
@@ -0,0 +1,9 @@
+
+  window.onload = function () {
+    document.getElementById("popup1").style.display = "block";
+  };
+
+  document.getElementById("fechar1").onclick = function () {
+    document.getElementById("popup1").style.display = "none";
+  };
+
