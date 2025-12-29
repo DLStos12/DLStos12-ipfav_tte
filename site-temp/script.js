8eaed03b852e94b26122f82de7bba135b475c712
DLStos12 <derick.stos@gmail.com>
2025-12-28 21:12:15 -0300

    #

---
+++ site-temp/script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.getElementById("popup1").style.display = "block";
+  };
+
+  document.getElementById("fechar1").onclick = function () {
+    document.getElementById("popup1").style.display = "none";
+  };
